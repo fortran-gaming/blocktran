1bf4ffce82158694d4084e0f345f9fb139ae5101
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-10-16 23:30:43 -0400

    doc [ci skip]

--- test_key.f90
+++ test_key.f90
@@ -1,20 +1,30 @@
 program test_key
 
   use cinter, only: initscr,getch, usleep
+  use, intrinsic:: iso_fortran_env, only: stdout=>output_unit, stdin=>input_unit
 
   implicit none
 
   integer :: ic
+  
+  !Close(stdin) first is not correct.
+  ! open(stdin,access='stream') ! error in gfortran. 
 
+  print *,'Ctrl-c to exit. Prints keys pressed and their code'
+  print *,'Special keys like arrows need nested "select case".'
+  call sleep(2)
+  
   call initscr()
-
-  print *,'Ctrl-c to stop program, which prints keys pressed and their code'
-  call usleep(2000000)
+  
 
   do
-    ic = getch()
-    print *,ic
-    call usleep(100000)
+    ic = getch()  ! 4-byte integer, automatically prints character!
+  ! read(stdin,*) ic !Nope
+    write(stdout,'(I4,A1)',advance='no') ic,char(13)
+    
+    call usleep(200000)
   end do
+  
+  call endwin()
 
 end program
