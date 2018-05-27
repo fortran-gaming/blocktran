2e8f2b49fcb63b3f57aaffe564c09b2a01ec85fc
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-05-27 13:39:20 -0400

    print stats

--- tests/randtest.f90
+++ tests/randtest.f90
@@ -7,7 +7,9 @@ implicit none
 character(*), parameter :: types = 'ITLJSZB'
 integer,parameter :: Ntypes=len(types)
 real, parameter :: rtol = 0.01
+real :: ideal
 character,allocatable :: b(:)
+real, allocatable :: e(:)
 integer :: i,n, c(Ntypes)
 character(32) :: buf
 
@@ -15,20 +17,29 @@ N=1000000
 call get_command_argument(1,buf,status=i)
 if (i==0) read(buf,*) N
 
-allocate(b(N))
+ideal = N/Ntypes
+
+allocate(b(N), e(N))
 
 call init_random_seed()
 
 call generate_next_type(b)
 
+! results
+print *,''
+print *,'ideal count:',int(ideal)
+print *,''
+print '(A5,A16,A10)','Block','Count','Error %'
+print '(A5,A16,A10)','-----','-----','-------'
 do i=1,Ntypes
 !do concurrent (i=1:Ntypes) ! even with ifort -parallel, still single core (with print commented)
   c(i) = count(b==types(i:i))
-  print *,types(i:i),c(i) 
+  e(i) = abs(c(i)-ideal) / ideal
+  print '(A5,I16,F10.3)',types(i:i), c(i), e(i)*100
 enddo
 
 ! randomness simple check -- sufficiently uniformly random
-if (any(abs(c-c(1)) / real(c(1)) > rtol)) then 
+if (any(e > rtol)) then 
   call err('non-uniform randomness posssible. Is N > 1000000?')
 endif
 
