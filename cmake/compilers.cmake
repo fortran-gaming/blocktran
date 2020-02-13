f716a7f27fa40ae8a6bb2d56b2838efdd60cf69d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-13 02:25:52 -0500

    cmake: cleanup options, use object libraries
    ci: don't test gui

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,30 +1,25 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  list(APPEND FFLAGS -march=native -Wall -Wextra -Wpedantic -Werror=array-bounds
-      -finit-real=nan -Wconversion -fimplicit-none) #-Warray-temporaries
+  add_compile_options(-march=native)
+  set(CMAKE_Fortran_FLAGS "-Wall -Wextra -Werror=array-bounds -finit-real=nan -Wconversion -fimplicit-none ")
 
-  if(CMAKE_BUILD_TYPE STREQUAL Debug)
-    list(APPEND FFLAGS -fexceptions -ffpe-trap=invalid,zero,overflow -fcheck=all)
-  endif()
+  set(CMAKE_Fortran_FLAGS_DEBUG "-fexceptions -ffpe-trap=invalid,zero,overflow -fcheck=all ")
 
   if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
-    list(APPEND FFLAGS -std=f2018)
+    string(APPEND CMAKE_Fortran_FLAGS "-std=f2018 ")
   endif()
 
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
   if(WIN32)
-    list(APPEND FFLAGS /traceback /warn /stand:f18)
+    set(CMAKE_Fortran_FLAGS "/traceback /warn /stand:f18 ")
   else()
-    list(APPEND FFLAGS -traceback -warn -stand f18)
+    set(CMAKE_Fortran_FLAGS "-traceback -warn -stand f18 ")
   endif()
 
-  if(CMAKE_BUILD_TYPE STREQUAL Debug)
-    list(APPEND FFLAGS -fpe0 -debug extended -check all)
-  endif()
+  set(CMAKE_Fortran_FLAGS_DEBUG "-fpe0 -debug extended -check all ")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL PGI)
-  set(FFLAGS -C -Mdclchk)
+  set(CMAKE_Fortran_FLAGS "-C -Mdclchk ")
 endif()
 
-# ---
 
 include(CheckFortranSourceCompiles)
 check_fortran_source_compiles("call random_init(.false., .false.); end" f18random SRC_EXT f90)
