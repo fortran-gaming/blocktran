8e1e9dda91a4f542682607bef06e6d3a518bf17c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-09 14:15:27 -0400

    cmake template

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,23 +1,25 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   add_compile_options(-march=native)
-  set(CMAKE_Fortran_FLAGS "-Wall -Wextra -Werror=array-bounds -finit-real=nan -Wconversion -fimplicit-none ")
+  string(APPEND CMAKE_Fortran_FLAGS " -Wall -Wextra -Werror=array-bounds -finit-real=nan -Wconversion -fimplicit-none")
 
-  set(CMAKE_Fortran_FLAGS_DEBUG "-fexceptions -ffpe-trap=invalid,zero,overflow -fcheck=all ")
+  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -fexceptions -ffpe-trap=invalid,zero,overflow -fcheck=all")
 
   if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
-    string(APPEND CMAKE_Fortran_FLAGS "-std=f2018 ")
+    string(APPEND CMAKE_Fortran_FLAGS " -std=f2018")
   endif()
 
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
   if(WIN32)
-    set(CMAKE_Fortran_FLAGS "/traceback /warn /stand:f18 ")
+    add_compile_options(/arch:native)
+    string(APPEND CMAKE_Fortran_FLAGS " /traceback /warn /stand:f18")
   else()
-    set(CMAKE_Fortran_FLAGS "-traceback -warn -stand f18 ")
+    add_compile_options(-march=native)
+    string(APPEND CMAKE_Fortran_FLAGS " -traceback -warn -stand f18")
   endif()
 
-  set(CMAKE_Fortran_FLAGS_DEBUG "-fpe0 -debug extended -check all ")
+  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -fpe0 -debug extended -check all")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL PGI)
-  set(CMAKE_Fortran_FLAGS "-C -Mdclchk ")
+  string(APPEND CMAKE_Fortran_FLAGS " -C -Mdclchk")
 endif()
 
 
