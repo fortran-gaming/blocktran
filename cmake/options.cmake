9635248e9ad06465ee46834395105f71a239a50a
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-05 13:52:53 -0400

    tested with CMake 3.14

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,6 +2,10 @@ message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION}  CMake ${CMAKE_VERSION}")
 
 option(${PROJECT_NAME}_BUILD_TESTING "enable testing" true)
 
+set(CI $ENV{CI})
+
+include(GNUInstallDirs)
+
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX ${PROJECT_BINARY_DIR} CACHE PATH "default install path" FORCE)
 endif()
