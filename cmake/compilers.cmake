6c5eebb2a8bc893860435e38ad05165d5aaf3036
scivision <scivision@users.noreply.github.com>
2023-02-22 02:06:24 -0500

    better CMake .gitignore

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -5,7 +5,6 @@ if(NOT abi_ok)
   try_compile(abi_ok
   ${CMAKE_CURRENT_BINARY_DIR}/abi_check ${CMAKE_CURRENT_LIST_DIR}/abi_check
   abi_check
-  OUTPUT_VARIABLE abi_log
   )
   if(abi_ok)
     message(CHECK_PASS "OK")
@@ -14,7 +13,6 @@ if(NOT abi_ok)
     C compiler ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION}
     C++ compiler ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}
     Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}
-    ${abi_log}
     "
     )
   endif()
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -10,7 +10,4 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX ${PROJECT_BINARY_DIR} CACHE PATH "default install path" FORCE)
 endif()
 
-# --- auto-ignore build directory
-if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
-  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
-endif()
+file(GENERATE OUTPUT .gitignore CONTENT "*")
