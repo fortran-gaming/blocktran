25fd9e56e9c3ad9be520b6466304b2e908fe94f1
scivision <scivision@users.noreply.github.com>
2025-02-07 07:30:00 -0500

    curses find enhancements

--- cmake/curses.cmake
+++ cmake/curses.cmake
@@ -5,20 +5,34 @@ if(find)
 endif()
 
 if(CURSES_FOUND)
-  if(NOT EXISTS ${CURSES_INCLUDE_DIRS}/curses.h)
+# CMake FindCurses may not find all needed include directories and may need definitions
+find_package(Curses REQUIRED)
+
+if(NOT CURSES_HAVE_CURSES_H)
+  foreach(d IN LISTS CURSES_INCLUDE_DIRS)
     find_path(curses_inc
     NAMES curses.h
-    HINTS ${CURSES_INCLUDE_DIRS}/ncurses ${CURSES_INCLUDE_DIRS}/pdcurses
-    REQUIRED
+    HINTS ${d}/ncurses ${d}/pdcurses
+    NO_DEFAULT_PATH
     )
-    list(APPEND CURSES_INCLUDE_DIRS ${curses_inc})
-  endif()
+    if(curses_inc)
+      list(APPEND CURSES_INCLUDE_DIRS ${curses_inc})
+      break()
+    endif()
+  endforeach()
+endif()
 
-  if(NOT TARGET CURSES::CURSES)
-    add_library(CURSES::CURSES INTERFACE IMPORTED)
-    target_link_libraries(CURSES::CURSES INTERFACE ${CURSES_LIBRARIES})
-    target_include_directories(CURSES::CURSES INTERFACE ${CURSES_INCLUDE_DIRS})
+if(NOT TARGET Curses::Curses)
+  add_library(Curses::Curses INTERFACE IMPORTED)
+  target_link_libraries(Curses::Curses INTERFACE ${CURSES_LIBRARIES})
+  target_include_directories(Curses::Curses INTERFACE ${CURSES_INCLUDE_DIRS})
+  if(CURSES_HAVE_NCURSES_H OR CURSES_HAVE_NCURSES_NCURSES_H)
+    # https://stackoverflow.com/a/67964252/4236751
+    # avoids MinGW link failure
+    target_compile_definitions(Curses::Curses INTERFACE $<$<BOOL:${MINGW}>:NCURSES_STATIC>)
   endif()
+endif()
+
 else()
 
   FetchContent_Declare(CURSES
